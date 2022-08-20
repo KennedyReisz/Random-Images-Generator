004ed46ccd8f3d30f5edc845867279a78079ed44
Kennedy <kennedyrecuperaodiscord@gmail.com>
2022-08-20 19:29:08 -0300

    Finished Duck and Fox request

--- pages/duck.jsx
+++ pages/duck.jsx
@@ -2,24 +2,23 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/rules-of-hooks */
-import Image from "next/image";
 import { useState } from "react";
 
 // Imported Asset's
 import styles from "../styles/Cat.module.css";
 
-export default function foxPage() {
-  const [fox, setFox] = useState([]);
+export default function catPage() {
+  const [duck, setDuck] = useState([]);
 
   const getDataFromApi = () => {
-    fetch("https://randomfox.ca/floof/")
+    fetch("https://random-duck.api.stdlib.com/api@1.2.1/quack/gif/")
       .then((response) => response.json())
-      .then((data) => setFox(data.image));
+      .then((data) => setDuck(data.url));
   };
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Fox Images Generator!</h1>
+      <h1 className={styles.title}>Duck Images Generator!</h1>
 
       <div className={styles.buttonsArea}>
         <button onClick={getDataFromApi} className={styles.generateButton}>
@@ -30,7 +29,7 @@ export default function foxPage() {
       </div>
 
       <div className={styles.imageGeneratedArea}>
-        <img src={fox} />
+        <img src={duck} />
       </div>
     </div>
   );
